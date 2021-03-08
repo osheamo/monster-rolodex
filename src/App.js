81210ff1f5187002c21d7879b768f0833216822d
Mark O Shea <osheamo@gmail.com>
2021-03-08 16:19:47 +0000

    adding searc-box component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box-component';
+
 import './App.css';
 
 class App extends Component {
@@ -18,16 +21,24 @@ class App extends Component {
 
   }
 
+  handleChange = (e) => (
+    this.setState({ searchField: e.target.value})
+  )
+
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => 
-      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+   const {monsters, searchField} = this.state;
+   const filteredMonsters = monsters.filter(monster => 
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
 
     return (
       <div className="App">
-        <input type='search' placeholder='Monster search' 
-          onChange={e => this.setState({ searchField: e.target.value }) }/>
-        <CardList monsters={filteredMonsters} />
+        <h1>Monster Rolodex</h1>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange = {this.handleChange}
+        />
+        <CardList monsters = {filteredMonsters} />
       </div>
     );
   }
